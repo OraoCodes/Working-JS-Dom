090d5058b9d28580aa3b2851bf8a7a8fcffbfe0d
Wycliffe Orao <oraowycliffe@students.uonbi.ac.ke>
2023-01-10 04:12:50 +0300

    use utility functions as opposed to drilling into the DOM

--- app.js
+++ app.js
@@ -1,2 +1,7 @@
 console.dir(document);
 document.body.children[1].children[0].children[0].children[0].href = 'https://www.google.com/';
+
+//alternatively we can use utility functions
+
+let anchorElement = document.getElementById('anchor-tag');
+anchorElement.href = "https://zemuldo.com/";
